ea3948baf6be10ed4b5b8dd3613d7178e6921b0b
hal39@SurfaceLaptop5 <hal39making@gmail.com>
2024-09-26 00:39:31 +0900

    laptop5 run.sh test

--- _job/run_config_3ax_zeros.sh
+++ _job/run_config_3ax_zeros.sh
@@ -1,11 +1,9 @@
 #$ -l rt_G.small=1
-#$ -l h_rt=12:00:00
+#$ -l h_rt=72:00:00
 #$ -N run_config_3ax_zeros
 #$ -o run_config_3ax_zeros.out
 #$ -e run_config_3ax_zeros.err
 #$ -cwd
-
-
 
 module load python/3.10/3.10.14 cuda/11.8/11.8.0 cudnn/8.6/8.6.0
 cd ~/repo/lab-abci
