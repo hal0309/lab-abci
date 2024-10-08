4d0693e1acb0b6925e80fad9948035e2430c9b94
hal39@SurfaceLaptop5 <hal39making@gmail.com>
2024-10-09 02:10:00 +0900

    laptop5

--- _job/run_config_3ax_zeros.sh
+++ _job/run_config_3ax_zeros.sh
@@ -1,12 +1,9 @@
 #$ -l rt_G.small=1
-#$ -l h_rt=72:00:00
-#$ -N run_config_3ax_zeros
-#$ -o run_config_3ax_zeros.out
-#$ -e run_config_3ax_zeros.err
+#$ -l h_rt=7:00:00
 #$ -cwd
 
 source /etc/profile.d/modules.sh
 module load python/3.10/3.10.14 cuda/11.8/11.8.0 cudnn/8.6/8.6.0
 cd ~/repo/lab-abci
 source ./env/bin/activate
-python train/train.py config_3ax_zeros.yaml 10
+python train/train.py config_3ax_zeros.yaml
