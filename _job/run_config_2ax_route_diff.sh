aa378ed03d576a38d0d68bfc6a2dc75cbc17ee57
hal39@SurfaceLaptop5 <hal39making@gmail.com>
2024-10-09 22:24:06 +0900

    laptop5 abci2ax準備

---
+++ _job/run_config_2ax_route_diff.sh
@@ -0,0 +1,9 @@
+#$ -l rt_G.small=1
+#$ -l h_rt=100:00:00
+#$ -cwd
+
+source /etc/profile.d/modules.sh
+module load python/3.10/3.10.14 cuda/11.8/11.8.0 cudnn/8.6/8.6.0
+cd ~/repo/lab-abci
+source ./env/bin/activate
+python train/train.py config_2ax_route_diff.yaml
