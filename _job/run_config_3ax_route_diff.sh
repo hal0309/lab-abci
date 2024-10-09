c233331023afc7322ae689942ca8e90c5dec2ca0
hal39@SurfaceLaptop5 <hal39making@gmail.com>
2024-10-09 22:39:14 +0900

    laptop5

--- _job/run_config_3ax_route_diff.sh
+++ _job/run_config_3ax_route_diff.sh
@@ -1,9 +1,9 @@
 #$ -l rt_G.small=1
-#$ -l h_rt=7:00:00
+#$ -l h_rt=72:00:00
 #$ -cwd
 
 source /etc/profile.d/modules.sh
 module load python/3.10/3.10.14 cuda/11.8/11.8.0 cudnn/8.6/8.6.0
 cd ~/repo/lab-abci
 source ./env/bin/activate
-python train/train.py config_3ax_zeros.yaml
+python train/train.py config_3ax_route_diff.yaml
