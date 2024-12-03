4bfcaa7e76d1d9cba02f003f006ee30d84c2c378
hal39@Desktop <hal39making@gmail.com>
2024-12-03 14:41:47 +0900

    home 経路長2,3,4,5,10,20,30,40

---
+++ run.bat
@@ -0,0 +1,10 @@
+@echo off
+py .\train\train.py config_p2.yaml
+py .\train\train.py config_p3.yaml
+py .\train\train.py config_p4.yaml
+py .\train\train.py config_p5.yaml
+py .\train\train.py config_p10.yaml
+py .\train\train.py config_p20.yaml
+py .\train\train.py config_p30.yaml
+py .\train\train.py config_p40.yaml
+pause
--- train/mylib/utils.py
+++ train/mylib/utils.py
@@ -17,31 +17,51 @@ def get_datetime() -> str:
     now = datetime.datetime.now(JST)
     return now.strftime('%Y-%m-%d-%H-%M')
 
+# dfの内容を配列にキャッシュ
+df_array = None
+
+def set_df_array(df):
+    global df_array
+
+    # 最大値を取得して2次元リストを初期化
+    max_x = df["x"].max()
+    max_y = df["y"].max()
+
+    df_array = [[None for _ in range(max_y + 1)] for _ in range(max_x + 1)]
+
+    # 2次元リストにデータを格納
+    for _, row in df.iterrows():
+        x = int(row["x"])  # 整数に変換
+        y = int(row["y"])
+        df_array[x][y] = (row["MF_X"], row["MF_Y"], row["MF_Z"])
+
 def get_mf(df, X, Y, is_2ax=False, add_noise=False):
     MF = []
+
+    if df_array is None:
+        print("Cache not found. Set cache.")
+        set_df_array(df)
+    
     for x, y in zip(X, Y):
         try:
             # MF.append(df[(df["x"] == x) & (df["y"] == y)]["MF"].values[0])
-            d = df[(df["x"] == x) & (df["y"] == y)]
-            x = d["MF_X"].values[0] 
-            y = d["MF_Y"].values[0]
-            z = d["MF_Z"].values[0]
+            mf_x, mf_y, mf_z = df_array[x][y]
 
             if add_noise:
                 mean = 0.0  # 平均
                 std = 0.381  # 標準偏差
-                x += np.random.normal(mean, std)
-                y += np.random.normal(mean, std)
-                z += np.random.normal(mean, std)
+                mf_x += np.random.normal(mean, std)
+                mf_y += np.random.normal(mean, std)
+                mf_z += np.random.normal(mean, std)
 
             if is_2ax:
-                horizontal = np.sqrt(x**2 + y**2)
-                virtical = z        
+                horizontal = np.sqrt(mf_x**2 + mf_y**2)
+                virtical = mf_z        
                 MF.append([horizontal, virtical])
             else:
-                MF.append([x, y, z])
+                MF.append([mf_x, mf_y, mf_z])
 
-        except:
-            print(f"Error: {x}, {y}")
+        except Exception as e:  # 例外の詳細を取得
+            print(f"Error at ({x}, {y}): {e}")
             MF.append(-1)
     return MF
