e35a4905be4a6860fbba8619d3a18b387f2f4e46
hal39@Desktop <hal39making@gmail.com>
2025-01-19 08:09:52 +0900

    home zerosでの比較(通常、divide)実行前

--- run.bat
+++ run.bat
@@ -1,11 +1,15 @@
 @echo off
-py .\train\train.py config_index_in5_d32_zeros.yaml 3000
-py .\train\train.py config_index_in5_d64_zeros.yaml 3000
-py .\train\train.py config_index_in5_d128_zeros.yaml 3000
-py .\train\train.py config_index_in5_d256_zeros.yaml 3000
-
+py .\train\train.py config_len_2_index_2ax_zeros.yaml 3000
+py .\train\train.py config_len_5_index_2ax_zeros.yaml 3000
+py .\train\train.py config_len_10_index_2ax_zeros.yaml 3000
+py .\train\train.py config_len_20_index_2ax_zeros.yaml 3000
+py .\train\train.py config_len_40_index_2ax_zeros.yaml 3000
+py .\train\train.py config_len_d20_index_2ax_zeros.yaml 1000
+py .\train\train.py config_len_d10_index_2ax_zeros.yaml 1000
+py .\train\train.py config_len_d5_index_2ax_zeros.yaml 1000
+py .\train\train.py config_len_d2_index_2ax_zeros.yaml 1000
 pause
 
-@REM py .\train\train.py config_flat_3ax_distance.yaml 
-@REM py .\train\train.py config_flat_3ax_route.yaml 
+@REM py .\train\train.py config_flat_3ax_distance.yaml
+@REM py .\train\train.py config_flat_3ax_route.yaml
 @REM py .\train\train.py config_flat_3ax_zeros.yaml
