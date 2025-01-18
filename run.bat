23f31abb86fdbc7bd42915bc0d2389623674b817
hal39@Desktop <hal39making@gmail.com>
2025-01-19 07:46:53 +0900

    home 5inでのモデルサイズ比較32,64,128,256

--- run.bat
+++ run.bat
@@ -1,6 +1,11 @@
 @echo off
-py .\train\train.py config_flat_2ax_zeros.yaml 
-py .\train\train.py config_flat_3ax_distance.yaml 
-py .\train\train.py config_flat_3ax_route.yaml 
-py .\train\train.py config_flat_3ax_zeros.yaml
+py .\train\train.py config_index_in5_d32_zeros.yaml 3000
+py .\train\train.py config_index_in5_d64_zeros.yaml 3000
+py .\train\train.py config_index_in5_d128_zeros.yaml 3000
+py .\train\train.py config_index_in5_d256_zeros.yaml 3000
+
 pause
+
+@REM py .\train\train.py config_flat_3ax_distance.yaml 
+@REM py .\train\train.py config_flat_3ax_route.yaml 
+@REM py .\train\train.py config_flat_3ax_zeros.yaml
