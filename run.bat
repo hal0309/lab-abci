4db4064a8b6c8326a68cda4c0f1c47097e493980
hal39@Desktop <hal39making@gmail.com>
2025-01-25 04:06:06 +0900

    home 作図

--- run.bat
+++ run.bat
@@ -1,5 +1,4 @@
 @echo off
-py .\train\train.py config_flat_2ax_zeros.yaml
 py .\train\train.py config_flat_3ax_distance.yaml
 py .\train\train.py config_flat_3ax_route.yaml
 py .\train\train.py config_flat_3ax_zeros.yaml
