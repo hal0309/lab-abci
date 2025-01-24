3d29612df1d366f10afb741ccde716537b5e8f61
hal39@Desktop <hal39making@gmail.com>
2025-01-24 10:27:44 +0900

    home 整理前

--- run.bat
+++ run.bat
@@ -1,15 +1,8 @@
 @echo off
-py .\train\train.py config_len_2_index_2ax_zeros.yaml 3000
-py .\train\train.py config_len_5_index_2ax_zeros.yaml 3000
-py .\train\train.py config_len_10_index_2ax_zeros.yaml 3000
-py .\train\train.py config_len_20_index_2ax_zeros.yaml 3000
-py .\train\train.py config_len_40_index_2ax_zeros.yaml 3000
-py .\train\train.py config_len_d20_index_2ax_zeros.yaml 1000
-py .\train\train.py config_len_d10_index_2ax_zeros.yaml 1000
-py .\train\train.py config_len_d5_index_2ax_zeros.yaml 1000
-py .\train\train.py config_len_d2_index_2ax_zeros.yaml 1000
+py .\train\train.py config_flat_2ax_zeros.yaml
+py .\train\train.py config_flat_3ax_distance.yaml
+py .\train\train.py config_flat_3ax_route.yaml
+py .\train\train.py config_flat_3ax_zeros.yaml
+py .\train\train.py config_flat2_3ax_zeros.yaml
 pause
 
-@REM py .\train\train.py config_flat_3ax_distance.yaml
-@REM py .\train\train.py config_flat_3ax_route.yaml
-@REM py .\train\train.py config_flat_3ax_zeros.yaml
